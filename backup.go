a39d2cc310c405a7e150ac1fe547c829b37af1af
Scott Miller <scott.miller171@gmail.com>
2016-03-09 15:42:26 -0500

    works without streaming

--- backup.go
+++ backup.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
-import "log"
-import "os/exec"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
 
 func main() {
 	path, err := exec.LookPath("mongodump")
@@ -19,4 +26,21 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(dumpOut))
+
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal("Failed to open file", err)
+	}
+
+	uploader := s3manager.NewUploader(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Body:   file,
+		Bucket: aws.String("net-openwhere-mongodb-snapshots-dev"),
+		Key:    aws.String("myKey"),
+	})
+	if err != nil {
+		log.Fatalln("Failed to upload", err)
+	}
+
+	log.Println("Successfully uploaded to", result.Location)
 }
