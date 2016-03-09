6b9dc5a4323a3be8e81a8cda816abb1575c958fe
Scott Miller <scott.miller171@gmail.com>
2016-03-09 12:00:12 -0500

    init

---
+++ backup.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "log"
+import "os/exec"
+
+func main() {
+	path, err := exec.LookPath("mongodump")
+	if err != nil {
+		log.Fatal("mongodump could not be found")
+	}
+	fmt.Printf("mongodump is available at %s\n", path)
+
+	dumpCmd := exec.Command("mongodump", "--host", "10.10.1.103", "port", "27017", "--archive=file.txt")
+
+	dumpOut, err := dumpCmd.Output()
+	fmt.Println(string(dumpOut))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dumpOut))
+}
