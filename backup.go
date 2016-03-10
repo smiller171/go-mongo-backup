6a411c332f260f778d9bc477494a63294454409e
Scott Miller <scott.miller171@gmail.com>
2016-03-10 13:59:22 -0500

    stream data instead of writing to file

--- backup.go
+++ backup.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
-	"os"
 	"os/exec"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -18,28 +18,42 @@ func main() {
 	}
 	fmt.Printf("mongodump is available at %s\n", path)
 
-	dumpCmd := exec.Command("mongodump", "--host", "10.10.1.103", "--port", "27017", "--archive=file.txt")
-
-	dumpOut, err := dumpCmd.Output()
-	fmt.Println(string(dumpOut))
+	dumpCmd := exec.Command(
+		"mongodump",
+		"--host",
+		"10.10.1.103",
+		"--port",
+		"27017",
+		"--archive"
+	)
+	body, err := dumpCmd.StdoutPipe()
 	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dumpOut))
-
-	file, err := os.Open("file.txt")
-	if err != nil {
-		log.Fatal("Failed to open file", err)
+		// handle error
 	}
 
-	uploader := s3manager.NewUploader(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	if err := dumpCmd.Start(); err != nil {
+		// handle error
+	}
+
+	// Wrap the pipe to hide the seek methods from the uploader
+	var bodyWrap = struct {
+		io.Reader
+	}{body}
+
+	uploader := s3manager.NewUploader(
+		session.New(&aws.Config{Region: aws.String("us-east-1")})
+	)
 	result, err := uploader.Upload(&s3manager.UploadInput{
-		Body:   file,
+		Body:   bodyWrap,
 		Bucket: aws.String("net-openwhere-mongodb-snapshots-dev"),
-		Key:    aws.String("myKey"),
+		Key:    aws.String("myKey2"),
 	})
 	if err != nil {
 		log.Fatalln("Failed to upload", err)
+	}
+
+	if err := dumpCmd.Wait(); err != nil {
+		// handle error
 	}
 
 	log.Println("Successfully uploaded to", result.Location)
