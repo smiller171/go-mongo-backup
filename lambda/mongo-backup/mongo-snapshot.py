8b694ded05158916efbcc2e7568d1de08826ea35
Scott Miller <scott@millergeek.xyz>
2016-12-02 14:09:37 -0500

    use environment vars for config and add docker-compose file for testing

--- lambda/mongo-backup/mongo-snapshot.py
+++ lambda/mongo-backup/mongo-snapshot.py
@@ -1,23 +1,13 @@
 import logging
-import boto3
 import json
 import requests
 import sys
 from os import environ
+from os import path
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
-
-dynamodb = boto3.resource('dynamodb', region_name=environ['AWS_DEFAULT_REGION'])
-
-# change this to whatever your table name is
-table = dynamodb.Table('mongo-backups')
-
-# I don't fully understand the reason for this. Following example
-# http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.Python.04.html
-pe = "#dmn, #pth, #bkt"
-ean = {"#dmn": "domain", "#pth": "path", "#bkt": "bucket"}
 
 
 # This is the method that will be registered
@@ -25,45 +15,39 @@ ean = {"#dmn": "domain", "#pth": "path", "#bkt": "bucket"}
 def handler(event={}, context={}):
     logger.info("started")
 
-    logger.info("scanning table")
-    nodes = table.scan(
-        ProjectionExpression=pe,
-        ExpressionAttributeNames=ean
-        )
+    domain = environ['URL'].strip('/ ')
+    bucket = environ['BUCKET'].strip()
+    basePath = environ['ROOT_PATH'].strip()
 
-    logger.info("nodes are " + str(nodes))
+    logger.info("bucket is " + bucket)
+    logger.info("base_path is " + basePath)
 
-    for i in nodes['Items']:
-        bucket = str(i['bucket'])
-        path = str(i['path'])
+    logger.info("setting mongodump json")
+    target = {
+        "bucket": bucket,
+        "path": path.join("/", basePath, "")
+    }
+    logger.info("mongodump json is " + json.dumps(target))
 
-        logger.info("bucket is " + str(i['bucket']))
-        logger.info("base_path is " + str(i['path']))
+    logger.info("setting url path")
+    url = path.join(domain, "v0/dump")
+    logger.info("url path is " + url)
 
-        logger.info("setting mongodump json")
-        target = {
-            "bucket": bucket,
-            "path": path
-        }
-        logger.info("mongodump json is " + json.dumps(target))
+    # trigger dump
+    logger.info("triggering dump")
+    try:
+        response = requests.post(
+            url,
+            data=json.dumps(target)
+            )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        logger.error(response.content)
+        sys.exit(1)
 
-        logger.info("setting url path")
-        url = i['domain'] + "/v0/dump"
-        logger.info("url path is " + url)
-
-        # trigger dump
-        logger.info("triggering dump")
-        try:
-            response = requests.post(
-                url,
-                data=json.dumps(target)
-                )
-        except requests.exceptions.RequestException as e:
-            logger.error(e)
-            sys.exit(1)
-
-        logger.info(response.content)
-        logger.info("new snapshot started at " + url)
+    logger.info(response.content)
+    logger.info("new snapshot started at " + url)
 
 
 # If being called locally, just call handler
