8d663272bdff3f5fa6154a6cd2be27ea1b4be19f
Scott Miller <scott@millergeek.xyz>
2017-03-07 12:18:38 -0500

    add prefix to rest endpoint

--- router.go
+++ router.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/gorilla/mux"
 )
 
 func newRouter() *mux.Router {
+	var prefix string
+	if os.Getenv("PREFIX") == "" {
+		prefix = "/mongo-backup"
+	} else {
+		prefix = path.Join("/", os.Getenv("PREFIX"))
+	}
 
 	router := mux.NewRouter().StrictSlash(true)
+	s := router.PathPrefix(prefix).Subrouter()
 	for _, route := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
 		handler = logger(handler, route.Name)
 
-		router.
+		s.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
 			Handler(handler)
 	}
 
-	return router
+	return s
 }
